9c7634aa8845b7f3bceb068e18971cf73deba4c1
Jason Holloway <jason.holloway@sorted.com>
2021-03-22 22:21:17 +0000

    fall back when not in space

--- lsp-cygwin-bodge.el
+++ lsp-cygwin-bodge.el
@@ -1,131 +1,191 @@
 
 (defun decyg (path)
-	"Coerce PATH to be Windows style."
-	(let* ((output (shell-command-to-string (concat "cygpath --windows '" path "'")))
-				 (trimmed (substring output 0 -1)))
-		trimmed
-		))
+  "Coerce PATH to be Windows style."
+  (let* ((output (shell-command-to-string (concat "cygpath --windows '" path "'")))
+         (trimmed (substring output 0 -1)))
+    trimmed
+    ))
+
+(defun recyg (path)
+  "Coerce PATH to be unix style."
+  (let* ((output (shell-command-to-string (concat "cygpath --unix '" path "'")))
+         (trimmed (substring output 0 -1)))
+    trimmed
+    ))
+
+(defun recyg-uri (uri)
+  "Convert URI back into Cygwin path."
+  (let ((path (substring uri 8)))
+    (recyg path)))
 
 (defun decyg-reslash (path)
-	"Desyg and reslash PATH."
-	(let* ((decyged (decyg path))
-				 (reslashed (s-replace "\\" "/" decyged))
-				 )
-		reslashed
-		))
+  "Desyg and reslash PATH."
+  (let* ((decyged (decyg path))
+         (reslashed (s-replace "\\" "/" decyged))
+         )
+    reslashed
+    ))
 
 (defun decyg-uri (path)
-	"Both decyg, reslash and urify PATH."
-	(let* ((reslashed (decyg-reslash path))
-				 (lowerCased (downcase reslashed))
-				 ;;(rehexed (url-hexify-string reslashed lsp--url-path-allowed-chars))
-				 (prefixed (concat "file:///" lowerCased))
-				 )
-		prefixed
-		))
+  "Both decyg, reslash and urify PATH."
+  (let* ((reslashed (decyg-reslash path))
+         (lowerCased (downcase reslashed))
+         ;;(rehexed (url-hexify-string reslashed lsp--url-path-allowed-chars))
+         (prefixed (concat "file:///" lowerCased))
+         )
+    prefixed
+    ))
 
-(defun mapIn (root uri)
-	(let* (
-				 (pathRoot (car root))
-				 (uriRoot (cdr root))
-				 (suffix (if (s-starts-with? uriRoot uri)
-						 (substring uri (length uriRoot))
-					 uri))
-				 (path (concat pathRoot suffix)))
-		path)
-	)
+(defun mangleIn (space uri)
+  "Read URI into SPACE."
+  (let* (
+         (pathRoots (car space))
+         (uriRoot (cdr space))
+         (path (if (s-starts-with? uriRoot uri)
+                   (let ((suffix (substring uri (length uriRoot))))
+                     (concat (first pathRoots) suffix))
+                 (recyg-uri uri)))
+         )
+    (message "%s <- %s" path uri)
+    path
+  ))
 
-(defun mapOut (root path)
-	(let* (
-				 (pathRoot (car root))
-				 (uriRoot (cdr root))
-				 (parts (pathFromRoot pathRoot path))
-				 (uri (concat uriRoot "/" (s-join "/" parts))))
-		uri)
-	)
+(defun mangleOut (space path)
+  (if-let (parts (pathFromRoot space path))
+      (let* (
+             (uriRoot (cdr space))
+             (uri (concat uriRoot "/" (s-join "/" parts)))
+             )
+        (message "%s -> %s" path uri)
+        uri)
+    (decyg-uri path)
+    ))
 
-(defun prepRoot (root)
-	"Prepare tuple from ROOT."
-	(let ((uri (decyg-uri root)))
-		(cons root uri))
-	)
-
-(defun pathFromRoot (root path)
-	(let* (
-				 (p (if (s-starts-with? root path)
-								(length root)
-							0))
-				 (suffix (substring path p))
-				 (split (pathToParts suffix)))
-		split))
+(defun ensureList (arg)
+  (if (listp arg) arg (list arg))
+  )
 
 (defun pathToParts (path)
-	(seq-filter
-	 (lambda (part) (not (s-equals? part "")))
-	 (s-split "/" path)))
+  (seq-filter
+   (lambda (part) (not (s-equals? part "")))
+   (s-split "/" path)))
 
 
+(defun prepSpace (arg)
+  "Prepare space tuple from ARG."
+  (let* (
+         (roots (ensureList arg))
+         (firstRoot (first roots))
+         (uri (decyg-uri firstRoot))
+         )
+    (cons roots uri))
+  )
+
+(defun pathFromRoot (space path)
+  "Find first matching root from SPACE for PATH; return the split relative path."
+  (let* (
+         (roots (car space))
+         (matchedRoot (seq-find (lambda (r) (s-starts-with? r path)) roots))
+         )
+    (when matchedRoot
+      (pathToParts (substring path (length matchedRoot))))))
+
+
+
+(ert-deftest recyg-uri-test ()
+  (should (equal
+           (recyg-uri "file:///c:/blah/moo.rs")
+           "/c/blah/moo.rs")))
 
 (ert-deftest pathToParts-test ()
-	(should (equal
-					 (pathToParts  "/home/jason/blah/moo.rs")
-					 '("home" "jason" "blah" "moo.rs"))))
+  (should (equal
+           (pathToParts  "/home/jason/blah/moo.rs")
+           '("home" "jason" "blah" "moo.rs"))))
 
 (ert-deftest pathFromRoot-test ()
-	(should (equal
-					 (PathFromRoot "/home/jason" "/home/jason/blah/moo.rs")
-					 '("blah" "moo.rs"))))
+  (should (equal
+           (pathFromRoot '(("/home/jason") "") "/home/jason/blah/moo.rs")
+           '("blah" "moo.rs"))))
 
-(ert-deftest prepRoot-test ()
-	(should (equal
-					 (prepRoot "/home/jason")
-					 '("/home/jason" . "file:///C%3A/msys/home/jason"))))
+(ert-deftest prepSpace-test ()
+  (should (equal
+           (prepSpace "/home/jason")
+           '(("/home/jason") . "file:///c:/msys/home/jason"))))
 
-(ert-deftest mapOut-test ()
-	(let* ((root (prepRoot "/home/jason"))
-				 (mapped (mapOut root "/home/jason/blah/moo.rs")))
-		(should (equal mapped "file:///C%3A/msys/home/jason/blah/moo.rs")))
-	)
+(ert-deftest mangleOut-test ()
+  (let* ((space (prepSpace "/home/jason"))
+         (mapped (mangleOut space "/home/jason/blah/moo.rs")))
+    (should (equal mapped "file:///c:/msys/home/jason/blah/moo.rs")))
+  )
 
-(ert-deftest mapIn-test ()
-	(let* ((root (prepRoot "/home/jason"))
-				 (path (mapIn root "file:///c:/msys/home/jason/blah/moo.rs")))
-		(should (equal path "/home/jason/blah/moo.rs")))
-	)
+(ert-deftest mangleOut2-test ()
+  (let* ((space (prepSpace "/home/jason"))
+         (mapped (mangleOut space "/c/src/kb/src/windows/mod.rs")))
+    (should (equal mapped "file:///c:/src/kb/src/windows/mod.rs")))
+  )
 
+(ert-deftest mangleIn-test ()
+  (let* ((space (prepSpace "/home/jason"))
+         (path (mangleIn space "file:///c:/msys/home/jason/blah/moo.rs")))
+    (should (equal path "/home/jason/blah/moo.rs")))
+  )
 
-(prepRoot "/home/jason")
-;; (mapIn (prepRoot "/home/jason") "file:///C%3A/msys/home/jason/blah/moo.rs")
-;; (mapOut (prepRoot "/home/jason") "/home/jason/one/two")
-;; (pathFromRoot "/home/jason" "/home/jason/one/two")
-;; (pathToParts "/hello")
-;; (rustic-flycheck-find-cargo-target "/home/jason/src/kb/src/main.rs")
+(ert-deftest mangleIn2-test ()
+  (let* ((space (prepSpace "/home/jason"))
+         (path (mangleIn space "file:///c:/src/kb/src/main.rs")))
+    (should (equal path "/c/src/kb/src/main.rs")))
+  )
+
 
 
 (setq lsp-log-io 1)
 
-(lexical-let ((root (prepRoot "/")))
+(lexical-let ((space (prepSpace '("/home/jason" "/c/Users/Jason.Holloway" "/c/msys/home/jason"))))
 
-	(defun mapOut-advice (path)
-		(mapOut root path))
+  (defun mangleOut-advice (path)
+    (mangleOut space path))
 
-	(advice-add 'lsp--path-to-uri-1 :override #'mapOut-advice)
-
-
-	(defun mapIn-advice (uri)
-		(mapIn root uri))
-
-	(advice-add 'lsp--uri-to-path :override #'mapIn-advice)
-
-	)
+  (advice-remove 'lsp--path-to-uri-1 'mangle)
+  (advice-add 'lsp--path-to-uri-1 :override #'mangleOut-advice '((name . mangle)))
 
 
-(defun decyg-args-advice (args)
-	"Blah ARGS."
-	(let* (
-				(path (car args))
-				(fixed (decyg path))
-				)
-		(list fixed)))
+  (defun mangleIn-advice (uri)
+    (mangleIn space uri))
 
-(advice-add 'rustic-flycheck-find-cargo-target :filter-args #'decyg-args-advice)
+  (advice-remove 'lsp--uri-to-path "mangle")
+  (advice-add 'lsp--uri-to-path :override #'mangleIn-advice '((name . mangle)))
+
+  )
+
+
+;; (defun decyg-args-advice (args)
+;;  "Blah ARGS."
+;;  (let* (
+;;        (path (car args))
+;;        (fixed (decyg path))
+;;        )
+;;    (list fixed)))
+
+
+(defun decyg-targets-advice (targets)
+  "Blah TARGETS."
+  (message "start")
+  (print targets)
+  (let* (
+         (oldPath (alist-get 'src_path (car (car targets))))
+         (newPath (recyg oldPath))
+         )
+    (setf (alist-get 'src_path (car (car targets))) newPath)
+    (print targets)
+    (message "stop")
+    targets
+    )
+  )
+
+(advice-remove 'rustic-flycheck-get-cargo-targets 'decyg)
+(advice-add 'rustic-flycheck-get-cargo-targets :filter-return #'decyg-targets-advice '((name . decyg)))
+
+
+;; (advice-remove 'rustic-flycheck-find-cargo-target 'decyg)
+;; (advice-add 'rustic-flycheck-find-cargo-target :filter-args #'decyg-args-advice '((name . decyg)))
+
